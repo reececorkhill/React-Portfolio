f48a5e50e578866a956e987dbd339458c29a4217
Reece <reece@smartit-webdevelopment.com>
2024-03-03 23:06:14 +0000

    Updated classes and for into className's and htmlFor's.

--- react-portfolio/src/components/Contact.jsx
+++ react-portfolio/src/components/Contact.jsx
@@ -5,15 +5,15 @@ const Contact = () => {
     <div className="row">
         <form>
               <div className="form-group">
-                <label for="FormControlInput1">Name</label>
+                <label htmlFor="FormControlInput1">Name</label>
                 <input type="text" className="form-control" id="FormControlInput1" placeholder="Enter Name"/>
               </div>
               <div className="form-group">
-                <label for="FormControlInput2">Email address</label>
+                <label htmlFor="FormControlInput2">Email address</label>
                 <input type="email" className="form-control" id="FormControlInput1" placeholder="Enter Email Address"/>
               </div>
               <div className="form-group">
-                <label for="FormControlTextarea1">Message</label>
+                <label htmlFor="FormControlTextarea1">Message</label>
                 <textarea className="form-control" id="FormControlTextarea1" rows="5" placeholder="Enter Message"></textarea>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
