da1e70bfc9d19c3b0a6fda29492e2e932d89443a
Reece <reece@smartit-webdevelopment.com>
2024-03-03 19:06:27 +0000

    Have removed unused files and imports.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   return (
