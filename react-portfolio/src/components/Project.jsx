8c871992db5da102ffa38f483008d2db13d544cf
Reece <reece@smartit-webdevelopment.com>
2024-03-03 20:24:59 +0000

    Added Project component and test content.

---
+++ react-portfolio/src/components/Project.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Project = () => {
+    return (
+    <div class="row work-row pt-5 pb-5">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="card mb-3" id="work-card">
+                <a href=""><img src="" class="card-img-top" alt="Project Image"/></a>
+                <div class="card-body">
+                    <h5 class="card-title">NJ Coaching Website</h5>
+                    <p class="card-text">A client website built using WordPress and Elementor.
+                    Fully responsive design that includes Image Galleries, a Contact Form,
+                    Google Analytics and a Google Maps Integration.
+                    </p>
+                    <p class="card-text"><small class="text-muted">WordPress</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Project;
