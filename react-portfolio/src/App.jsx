179b282a72ba302eeb958bf3b8fce735252502d6
Reece <reece@smartit-webdevelopment.com>
2024-03-03 23:43:58 +0000

    Updated Projects(ProjectGallery.jsx) file to render Project component.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
+import ProjectGallery from './components/ProjectGallery.jsx';
 import Project from './components/Project.jsx'
 import Resume from './components/Resume.jsx'
 import Contact from './components/Contact.jsx'
@@ -11,9 +12,9 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Projects" element={<Project />} />
+        <Route path="/Projects" element={<ProjectGallery />} />
         <Route path="/Resume" element={<Resume />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -16,6 +16,45 @@ const Project = () => {
                 </div>
             </div>
         </div>
+        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div className="card mb-3" id="work-card">
+                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
+                <div className="card-body">
+                    <h5 className="card-title">NJ Coaching Website</h5>
+                    <p className="card-text">A client website built using WordPress and Elementor.
+                    Fully responsive design that includes Image Galleries, a Contact Form,
+                    Google Analytics and a Google Maps Integration.
+                    </p>
+                    <p className="card-text"><small className="text-muted">WordPress</small></p>
+                </div>
+            </div>
+        </div>
+        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div className="card mb-3" id="work-card">
+                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
+                <div className="card-body">
+                    <h5 className="card-title">NJ Coaching Website</h5>
+                    <p className="card-text">A client website built using WordPress and Elementor.
+                    Fully responsive design that includes Image Galleries, a Contact Form,
+                    Google Analytics and a Google Maps Integration.
+                    </p>
+                    <p className="card-text"><small className="text-muted">WordPress</small></p>
+                </div>
+            </div>
+        </div>
+        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div className="card mb-3" id="work-card">
+                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
+                <div className="card-body">
+                    <h5 className="card-title">NJ Coaching Website</h5>
+                    <p className="card-text">A client website built using WordPress and Elementor.
+                    Fully responsive design that includes Image Galleries, a Contact Form,
+                    Google Analytics and a Google Maps Integration.
+                    </p>
+                    <p className="card-text"><small className="text-muted">WordPress</small></p>
+                </div>
+            </div>
+        </div>
     </div>
     );
 };
