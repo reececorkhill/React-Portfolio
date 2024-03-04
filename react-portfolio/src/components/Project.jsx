ab16d90448d20e0813ffba3ca22a16d22cd8ba18
Reece <reece@smartit-webdevelopment.com>
2024-03-04 01:51:28 +0000

    Added content to projects.json and now passing and display props data.

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,58 +1,14 @@
 import React from 'react';
 
-const Project = () => {
+const Project = (props) => {
     return (
-    <div className="row work-row pt-5 pb-5">
-        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <div className="card mb-3" id="work-card">
-                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
-                <div className="card-body">
-                    <h5 className="card-title">NJ Coaching Website</h5>
-                    <p className="card-text">A client website built using WordPress and Elementor.
-                    Fully responsive design that includes Image Galleries, a Contact Form,
-                    Google Analytics and a Google Maps Integration.
-                    </p>
-                    <p className="card-text"><small className="text-muted">WordPress</small></p>
-                </div>
-            </div>
-        </div>
-        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <div className="card mb-3" id="work-card">
-                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
-                <div className="card-body">
-                    <h5 className="card-title">NJ Coaching Website</h5>
-                    <p className="card-text">A client website built using WordPress and Elementor.
-                    Fully responsive design that includes Image Galleries, a Contact Form,
-                    Google Analytics and a Google Maps Integration.
-                    </p>
-                    <p className="card-text"><small className="text-muted">WordPress</small></p>
-                </div>
-            </div>
-        </div>
-        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <div className="card mb-3" id="work-card">
-                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
-                <div className="card-body">
-                    <h5 className="card-title">NJ Coaching Website</h5>
-                    <p className="card-text">A client website built using WordPress and Elementor.
-                    Fully responsive design that includes Image Galleries, a Contact Form,
-                    Google Analytics and a Google Maps Integration.
-                    </p>
-                    <p className="card-text"><small className="text-muted">WordPress</small></p>
-                </div>
-            </div>
-        </div>
-        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <div className="card mb-3" id="work-card">
-                <a href=""><img src="" className="card-img-top" alt="Project Image"/></a>
-                <div className="card-body">
-                    <h5 className="card-title">NJ Coaching Website</h5>
-                    <p className="card-text">A client website built using WordPress and Elementor.
-                    Fully responsive design that includes Image Galleries, a Contact Form,
-                    Google Analytics and a Google Maps Integration.
-                    </p>
-                    <p className="card-text"><small className="text-muted">WordPress</small></p>
-                </div>
+    <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div className="card mb-3" id="work-card">
+            <a href=""><img src="" className="card-img-top" alt={props.imgalt}/></a>
+            <div className="card-body">
+                <h5 className="card-title">{props.h5}</h5>
+                <p className="card-text">{props.p1}</p>
+                <p className="card-text"><small className="text-muted">{props.tag}</small></p>
             </div>
         </div>
     </div>
--- react-portfolio/src/components/ProjectGallery.jsx
+++ react-portfolio/src/components/ProjectGallery.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Project from './Project';
+import projectData from '../../data/projects.json'
 
 const ProjectGallery = () => {
     return (
     <div className="row work-row pt-5 pb-5">
-        <Project />
+        {projectData.map((data) => (
+            <Project
+                key={data.id}
+                imgsource={data.imgsource}
+                imgalt={data.imgalt}
+                h5={data.h5}
+                p1={data.p1}
+                tag={data.tag}
+            />
+        ))}
     </div>
     );
 };
