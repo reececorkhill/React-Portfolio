ee25479a0de6953eecf03afd5be37ee1fc370f04
Reece <reece@smartit-webdevelopment.com>
2024-03-07 00:42:23 +0000

    Added buttons to About page with links to pages.

--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
     <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6">
         {aboutData.map((data) => (
-           <div className="card mb-3" id="about-card" key={"About"}>
+           <div id="about-card" key={"About"}>
            <img src={data.imgsource} className="card-img-top" alt={data.imgalt}/>
                <div className="card-body">
                    <h1>{data.h1}</h1>
@@ -18,6 +18,8 @@ const About = () => {
                    <p className="card-text card-text-left">
                    {data.p3}
                    </p>
+                   <a className="btn btn-primary btn-lg" href="/Projects" role="button">View Projects</a>
+                   <a className="btn btn-primary btn-lg" href="/Resume" role="button">View Resume</a>
                </div>
            </div> 
         ))}
