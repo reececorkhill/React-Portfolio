aa90da87c7f42d82f3d4fbcf86e77c9a60469073
Reece <reece@smartit-webdevelopment.com>
2024-03-16 00:11:33 +0000

    Updated project and gallery components to now render correctly.

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import projectData from '../../data/projects.json';
 
-const Project = () => {
+const Project = (props) => {
     return (
-        <div className="row project-row pt-5 pb-5">
-            {projectData.map((data) => (
-                <div className="project-cards-div col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                    <div className="card mb-3">
-                        <img src={data.imgsource} className="card-img-top" alt={data.imgalt}/>
-                        <div className="card-body">
-                            <h2>{data.h2}</h2>
-                            <p className="card-text card-text-left">
-                            {data.p1}
-                            </p>
-                            <div className="project-card-buttons">
-                                <Link to={`/Projects/${data.id}`}>
-                                    <button className="btn btn-outline-light btn-md">Read More...</button>
-                                </Link>
-                                <a className="btn btn-outline-light btn-md" href={data.repo} role="button">GitHub</a>
-                                <a className="btn btn-outline-light btn-md" href={data.demo} role="button">Demo</a>
-                            </div>
-                            <p className="card-text"><small>{data.tag}</small></p>
-                        </div>
-                    </div> 
+    <>
+    <div className="project-cards-div col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <div className="card mb-3">
+            <img src={props.imgsource} className="card-img-top" alt={props.imgalt}/>
+            <div className="card-body">
+                <h2>{props.h2}</h2>
+                <p className="card-text card-text-left">
+                {props.p1}
+                </p>
+                <div className="project-card-buttons">
+                    <Link to={`/Projects/${props.id}`}>
+                        <button className="btn btn-outline-light btn-md">Read More...</button>
+                    </Link>
+                    <a className="btn btn-outline-light btn-md" href={props.repo} role="button">GitHub</a>
+                    <a className="btn btn-outline-light btn-md" href={props.demo} role="button">Demo</a>
                 </div>
-            ))}
+                <p className="card-text"><small>{props.tag}</small></p>
+            </div>
         </div>
+    </div>
+    </>
     );
 };
 
