5b80e5af1b988b7a28c95d089b4d620463774a50
Reece <reece@smartit-webdevelopment.com>
2024-03-18 18:08:42 +0000

    Using HashRouter instead of just Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
